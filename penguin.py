2c0259f2342499538bb428017fb2a76082cb8297
Ekaterina Pischyota <lady.pischyota@yandex.ru>
2025-03-05 21:07:59 +0500

    Добавили управление пингвином влево вправо

--- penguin.py
+++ penguin.py
@@ -1,3 +1,6 @@
+import curses
+
+# ASCII-арт пингвина
 penguin = r"""
        .--.  
       |o_o |  
@@ -8,4 +11,39 @@ penguin = r"""
    \___)=(___/  
 """
 
-print(penguin)
+def main(stdscr):
+    # Очищаем экран и отключаем курсор
+    curses.curs_set(0)
+    stdscr.clear()
+    
+    # Получаем начальные размеры окна
+    max_y, max_x = stdscr.getmaxyx()
+
+    # Начальная позиция пингвина
+    x = max_x // 2 - len(penguin.splitlines()[0]) // 2
+    y = max_y // 2 - len(penguin.splitlines()) // 2
+    
+    while True:
+        # Очищаем экран
+        stdscr.clear()
+
+        # Рисуем пингвина в новой позиции
+        for i, line in enumerate(penguin.splitlines()):
+            stdscr.addstr(y + i, x, line)
+        
+        # Обновляем экран
+        stdscr.refresh()
+
+        # Получаем ввод с клавиатуры
+        key = stdscr.getch()
+
+        # Управление движением пингвина
+        if key == curses.KEY_RIGHT:
+            x += 1  # Двигаем вправо
+        elif key == curses.KEY_LEFT:
+            x -= 1  # Двигаем влево
+        elif key == ord('q'):
+            break  # Выход из программы при нажатии 'q'
+
+# Инициализация curses
+curses.wrapper(main)
